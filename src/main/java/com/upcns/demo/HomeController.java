61df3d4d3c51eb5e4b56f75c719ac2c9ba2a07db
nothorse <nothorse@gmail.com>
2025-08-04 16:38:31 +0900

    객체 리런, 생성자,getter사용필요. 단lombok 사용시 생략가능

--- src/main/java/com/upcns/demo/HomeController.java
+++ src/main/java/com/upcns/demo/HomeController.java
@@ -1,9 +1,13 @@
 package com.upcns.demo;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -32,5 +36,51 @@ public class HomeController {
     public int plus2(int a,@RequestParam(defaultValue = "0") int b ){
         System.out.println("b:" + b);
         return a+b;
+    }
+
+
+    @GetMapping("/mapList")
+    @ResponseBody
+    //
+    public Map<String,Object> mapList(){
+        Map<String,Object> map = new HashMap<>(){{
+            put("james","test");
+            put("james",new Car(1,"james","blue"));
+            put("james2",new Car(2,"james2","red"));
+            put("james3",new Car2(2,"james2","red"));
+        }};
+        return map;
+    }
+
+    class Car{
+        private final int id;
+        private final String name;
+        private final String color;
+
+        public Car(int id, String name, String color) {
+            this.id = id;
+            this.name = name;
+            this.color = color;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getColor() {
+            return color;
+        }
+    }
+    @AllArgsConstructor
+    //Lombok이 있으면 생략가능 (생성자,getter)
+    class Car2{
+        private final int id;
+        private final String name;
+        private final String color;
+
     }
 }
