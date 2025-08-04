6295026fb1dfe04d43fab8a32fe3b73ff97c7124
nothorse <nothorse@gmail.com>
2025-08-04 16:17:01 +0900

    파라메터 이름으로 자동 반영

--- src/main/java/com/upcns/demo/HomeController.java
+++ src/main/java/com/upcns/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.upcns.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -22,5 +23,14 @@ public class HomeController {
     public int plus(){
         counter++;
         return counter;
+    }
+    @GetMapping("/plus2")
+    @ResponseBody
+    //public int plus2(int a, int b ){ //b가 파라메터로 안오면 에러
+    //http://localhost:8080/plus2?a=1&b=20
+    //http://localhost:8080/plus2?a=1
+    public int plus2(int a,@RequestParam(defaultValue = "0") int b ){
+        System.out.println("b:" + b);
+        return a+b;
     }
 }
